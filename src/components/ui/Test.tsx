8e6c36fa27df909c4dbd56821fc95f9963b273c2
Denys <babychdenya@gmail.com>
2024-02-02 16:36:06 +0000

    chore: resolve conflicts

--- src/components/ui/Test.tsx
+++ src/components/ui/Test.tsx
@@ -1,16 +1,17 @@
 import { useTheme } from 'next-themes'
 
 export const Test = () => {
-    const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
 
-    return <div>
+  return (
+    <div>
       The current theme is: {theme}
       <button onClick={() => setTheme('light')}>Light Mode</button>
-        <button onClick={() => setTheme('dark')}>Dark Mode</button>
-        <button onClick={() => setTheme('violet')}>Violet Mode</button>
+      <button onClick={() => setTheme('dark')}>Dark Mode</button>
+      <button onClick={() => setTheme('violet')}>Violet Mode</button>
     </div>
+  )
 }
-
 
 // import { useTheme } from 'next-themes'
 
