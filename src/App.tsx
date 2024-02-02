18e8268595be42b378c8413c5fcc27db0095bd30
Denys <babychdenya@gmail.com>
2024-02-02 23:26:08 +0000

    feat: add input reusable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Button } from 'components/ui/Button'
+import { Field } from 'components/ui/Field'
 import { Test } from 'components/ui/Test'
 import { ThemeProvider } from 'next-themes'
 
 function App() {
   return (
-    <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
-      <Test />
-    </ThemeProvider>
+    <>
+      <Button size='sm'></Button>
+      <Field inputName='name' placeholder='Enter name' width='md' />
+      <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
+        <Test />
+      </ThemeProvider>
+    </>
   )
 }
 
