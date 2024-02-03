826fe702867c98554b19583eea4e5c9db8bca375
Sergii-Drozdiuk <drozduyk@gmail.com>
2024-02-03 20:35:06 +0200

    feat: create HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Button } from 'components/ui/button/Button'
 import { ThemeProvider } from 'next-themes'
 import { AuthPage } from 'pages/AuthPage'
+import { HomePage } from 'pages/HomePage'
 
 function App() {
   return (
     <>
       <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
         <Button isAddIcon>Add</Button>
+        <HomePage />
         <AuthPage />
       </ThemeProvider>
     </>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,43 @@
+import { Link } from "@tanstack/react-router"
+
 export const HomePage = () => {
   return (
-    // <Sidebar>
-    // <SidebarCreateBoardBtn></SidebarCreateBoardBtn>
-    // <SidebarBoardsList>
-    //   <SideBarBoardsItem></SideBarBoardsItem>
-    //     </SidebarBoardsList>
-    // <SidebarUserSupport></SidebarUserSupport>
-    //   <SidebarLogoutBtn></SidebarLogoutBtn>
-    // </Sidebar>
-    // <Header>
-    //   <HeaderThemeSelect></HeaderThemeSelect>
-    //   <HeaderUserInfo></HeaderUserInfo>
-    // </Header>
-    // <Filters/>
-    // <Board>
-    //   <BoardList>
-    //     <BoardItem>
-    //       <BoardHeadingList>
-    //         <BoardHeadingItem></BoardHeadingItem>
-    //       </BoardHeadingList>
-    //       <BoardTasksList>
-    //         <BoardTaskItem></BoardTaskItem>
-    //       </BoardTasksList>
-    //     </BoardItem>
-    //   </BoardList>
-    // </Board>
-    <div></div>
+    <div className='bg-welcome-page-gradient'>
+      <div className='flex justify-center'>
+      <picture>
+              <source
+                media="(min-width: 768px)"
+          srcSet="
+                  /images/boy_t_d.avif 1x, 
+                  /images/boy_t_d@2x.avif 2x,
+                  /images/boy_t_d@3x.avif 3x
+                "
+              />
+              
+              <source
+                media="(max-width: 767px)"
+                srcSet="
+                  /images/boy_phone.avif 1x, 
+                  /images/boy_phone@2x.avif 2x,
+                  /images/boy_phone@3x.avif 3x
+                "
+              />
+              
+              <img
+                className="user-with-notebook"
+                src="images/boy_phone.avif"
+                alt="user-with-notebook"
+                loading="lazy"
+              />
+        </picture>
+        </div>
+      <h1>Task Pro</h1>
+      <p>Supercharge your productivity and take control of your tasks with Task Pro - Don't wait, start achieving your goals now!</p>
+      <ul>
+        
+        <li><Link to='/signup'>Registration</Link> </li>
+        <li><Link to='/signin'>Log In</Link> </li>
+      </ul>
+    </div>
   )
 }
