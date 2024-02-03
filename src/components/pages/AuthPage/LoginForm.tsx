90251616618db6f18a667de46b62b3c2013e9f74
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-03 20:48:56 +0200

    feat: added valibot validation

--- src/components/pages/AuthPage/LoginForm.tsx
+++ src/components/pages/AuthPage/LoginForm.tsx
@@ -1,13 +1,31 @@
 import { useForm } from 'react-hook-form'
 import { Button } from 'components/ui/button/Button'
+import { valibotResolver } from '@hookform/resolvers/valibot'
+import { object, email, string, minLength, toTrimmed, maxLength } from 'valibot'
 
 type FormFields = {
   email: string
   password: string
 }
 
+const LoginSchema = object({
+  email: string([toTrimmed(), email('Please enter a valid email.')]),
+  password: string('password is required', [
+    toTrimmed(),
+    minLength(8, 'Password should be at least 8 characters'),
+    maxLength(64, 'Password is too long')
+  ])
+})
+
 export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<FormFields>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormFields>({
+    resolver: valibotResolver(LoginSchema),
+    shouldUseNativeValidation: false
+  })
 
   const onSubmit = (data: FormFields) => {
     console.log(data)
@@ -15,15 +33,21 @@ export const LoginForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
-        {...register('email', { required: true })}
+        {...register('email')}
         type='email'
         placeholder='Enter your email'
       />
+      {errors.email && (
+        <div className='text-red-500'>{errors.email.message}</div>
+      )}
       <input
-        {...register('password', { required: true })}
+        {...register('password')}
         type='password'
-        placeholder='Create a password'
+        placeholder='Confirm a password'
       />
+      {errors.password && (
+        <div className='text-red-500'>{errors.password.message}</div>
+      )}
       <Button type='submit'>Log In Now</Button>
     </form>
   )
--- src/components/pages/AuthPage/RegisterForm.tsx
+++ src/components/pages/AuthPage/RegisterForm.tsx
@@ -1,5 +1,7 @@
 import { Button } from 'components/ui/button/Button'
 import { useForm } from 'react-hook-form'
+import { valibotResolver } from '@hookform/resolvers/valibot'
+import { object, email, string, minLength, toTrimmed, maxLength } from 'valibot'
 
 type FormFields = {
   name: string
@@ -7,8 +9,29 @@ type FormFields = {
   password: string
 }
 
+const RegisterSchema = object({
+  name: string([
+    toTrimmed(),
+    minLength(2, 'Name should be at least 2 characters'),
+    maxLength(32, 'Name is too long')
+  ]),
+  email: string([toTrimmed(), email('Please enter a valid email.')]),
+  password: string([
+    toTrimmed(),
+    minLength(8, 'Password should be at least 8 characters'),
+    maxLength(64, 'Password is too long')
+  ])
+})
+
 export const RegisterForm = () => {
-  const { register, handleSubmit } = useForm<FormFields>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormFields>({
+    resolver: valibotResolver(RegisterSchema),
+    shouldUseNativeValidation: false
+  })
 
   const onSubmit = (data: FormFields) => {
     console.log(data)
@@ -16,21 +39,24 @@ export const RegisterForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('name')} type='text' placeholder='Enter your name' />
+      {errors.name && <div className='text-red-500'>{errors.name.message}</div>}
       <input
-        {...register('name', { required: true })}
-        type='text'
-        placeholder='Enter your name'
-      />
-      <input
-        {...register('email', { required: true })}
+        {...register('email')}
         type='email'
         placeholder='Enter your email'
       />
+      {errors.email && (
+        <div className='text-red-500'>{errors.email.message}</div>
+      )}
       <input
-        {...register('password', { required: true })}
+        {...register('password')}
         type='password'
         placeholder='Create a password'
       />
+      {errors.password && (
+        <div className='text-red-500'>{errors.password.message}</div>
+      )}
       <Button type='submit'>Regiter Now</Button>
     </form>
   )
