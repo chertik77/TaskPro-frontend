3bb36ad2aa910c5008b62895d0301fea89d80a52
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-02-04 15:50:24 +0100

    feat: add icons to a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Link, Outlet } from '@tanstack/react-router'
+import { Button } from 'components/ui/button/Button'
 import { AuthPage } from 'pages/AuthPage'
 
 function App() {
   return (
     <>
+      <Button isSmallIcon iconName='arrow' />
       <AuthPage />
       <Outlet />
       <Link to='dashboard'>dasboard</Link>
