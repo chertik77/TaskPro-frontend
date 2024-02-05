f6ed2a2abff800aec5a5f9f725252ac574b4d29c
Denys <babychdenya@gmail.com>
2024-02-05 21:51:20 +0000

    refactor: add basename to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
           <BrowserRouter
-            basename={import.meta.env.DEV ? '/' : 'final-project-frontend'}>
+            basename={import.meta.env.DEV ? '/' : '/final-project-frontend'}>
             <App />
           </BrowserRouter>
         </ThemeProvider>
