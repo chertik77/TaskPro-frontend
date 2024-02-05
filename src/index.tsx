1045555caa1806cb14f127fac59c5b02127fc744
Denys <babychdenya@gmail.com>
2024-02-05 14:23:14 +0000

    refactor: remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,12 @@ import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
 
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,27 @@
 import { Link } from '@tanstack/react-router'
-import { Logo, Picture } from 'components/pages/HomePage/Images'
 
 export const HomePage = () => (
   <div className='h-dvh adaptive:px-5 flex justify-center items-center flex-col bg-welcome-page-gradient'>
-    <Picture />
+    <picture>
+      <source
+        media='(min-width: 768px)'
+        srcSet='/images/Welcome_tab_desktop.avif 1x, /images/Welcome_tab_desktop@2x.avif 2x'
+      />
+      <source
+        media='(max-width: 767px)'
+        srcSet='/images/Welcome_phone.avif 1x, /images/Welcome_phone@2x.avif 2x'
+      />
+      <img
+        className='user-with-notebook'
+        src='images/Welcome_tab_desktop.avif'
+        alt='user-with-notebook'
+        loading='lazy'
+      />
+    </picture>
     <div className='flex gap-[14px] items-center mt-6'>
-      <Logo />
+      <svg className='w-10 h-10 tablet:w-12 tablet:h-12'>
+        <use xlinkHref='/assets/icons.svg#icon-logo' />
+      </svg>
       <h1 className='text-fs-28-lh-normal-fw-600 tablet:text-fs-40-lh-normal-fw-600 text-black'>
         Task Pro
       </h1>
