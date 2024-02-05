c25a22cef51fd91ea67534f98ea8c48ca2b2da7d
Denys <babychdenya@gmail.com>
2024-02-05 13:11:19 +0000

    fix(contet): fix center content on mobile

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -2,7 +2,7 @@ import { AuthFormNavigation } from 'components/pages/AuthFormNavigation'
 import { SigninForm } from 'components/pages/SigninForm'
 
 export const SigninPage = () => (
-  <div className='h-screen flex items-center justify-center adaptive:px-5 bg-welcome-page-gradient'>
+  <div className='h-dvh flex items-center justify-center adaptive:px-5 bg-welcome-page-gradient'>
     <div className='rounded-lg bg-black w-[335px] p-6 tablet:w-[424px] tablet:p-10'>
       <AuthFormNavigation />
       <SigninForm />
