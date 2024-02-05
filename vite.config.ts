3482c36ac5faad9538b134f065c0f6947a127dbb
Denys <babychdenya@gmail.com>
2024-02-05 00:00:22 +0000

    fix: added base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
   server: {
     port: 3000
-  }
+  },
+  base: 'https://final-project-frontend-theta.vercel.app/'
 })
