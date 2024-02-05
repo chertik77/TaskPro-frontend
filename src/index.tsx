8650981d466f7bd71ded1062a9d5b134bbcc8074
MorskoySom <Somovvitvit@gmail.com>
2024-02-05 21:50:05 +0200

    feat: create api call for current user

--- src/index.tsx
+++ src/index.tsx
@@ -3,22 +3,27 @@ import { ThemeProvider } from 'next-themes'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from 'redux/store'
+import { store, persistor } from 'redux/store'
 import { Toaster } from 'sonner'
 import './index.css'
 import { routeTree } from './routeTree.gen'
+
+import { PersistGate } from 'redux-persist/integration/react'
 
 const router = createRouter({
   routeTree
 })
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
-        <RouterProvider router={router} />
-        <Toaster richColors position='top-right' />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider attribute='class' themes={['light', 'dark', 'violet']}>
+          <RouterProvider router={router} />
+          <Toaster richColors position='top-right' />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
