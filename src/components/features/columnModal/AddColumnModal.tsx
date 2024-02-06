381647c723af30d9436831aa6d9a66a80243353b
Valentun2 <pocomaxawrkt@gmail.com>
2024-02-06 22:54:06 +0200

    chore: add column modals

---
+++ src/components/features/columnModal/AddColumnModal.tsx
@@ -0,0 +1,28 @@
+import { Button, Field, Modal } from 'components/ui'
+import { useModal } from 'hooks'
+import { useBoard } from 'hooks/useBoard'
+
+export const AddColumnModal = () => {
+  const { isModalOpen, toggleModal } = useModal()
+
+  const { register, errors } = useBoard()
+  return (
+    <Modal
+      size='sm'
+      modalTitle='Add column'
+      isModalOpen={isModalOpen}
+      onCloseModal={toggleModal}>
+      <Field
+        {...register('title')}
+        inputName='title'
+        placeholder='Title'
+        errors={errors}
+        className='mb-6'
+      />
+
+      <Button isAddIcon={true} onClick={e => console.log(e)}>
+        Add
+      </Button>
+    </Modal>
+  )
+}
