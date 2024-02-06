1aa8497f20528d449db8de40953e520333811bfe
Denys <babychdenya@gmail.com>
2024-02-06 23:34:33 +0000

    feat: add new library to control modal state

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useAppDispatch'
 export * from './useAuth'
 export * from './useIsFormValidOnReload'
-export * from './useModal'
 export * from './useSigninForm'
 export * from './useSignupForm'
