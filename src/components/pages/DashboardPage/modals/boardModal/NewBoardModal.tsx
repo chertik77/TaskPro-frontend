982dea4f76ef7e68c179493a89b0ca67ad8bbd38
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-02-08 03:26:35 +0100

    feat: create api call to the server to create new board

--- src/components/pages/DashboardPage/modals/boardModal/NewBoardModal.tsx
+++ src/components/pages/DashboardPage/modals/boardModal/NewBoardModal.tsx
@@ -6,43 +6,60 @@ import type { ChangeEvent } from 'react'
 import { BackgroundContainer } from './BackgroundContainer'
 import { Icons } from './Icons'
 import { useAddNewBoardMutation } from 'redux/api/dashboard/board'
+import { useState } from 'react'
 
 const NewBoardModal = () => {
   const { register, errors } = useBoard()
-  const [addNewBoard, { isLoading }] = useAddNewBoardMutation()
+  const [addNewBoard] = useAddNewBoardMutation()
+
+  const [formData, setFormData] = useState({
+    title: '',
+    icon: '',
+    background: ''
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+  console.log(formData)
+
+  const handleCreateBoard = () => {
+    addNewBoard({
+      title: formData.title,
+      icon: formData.icon, // одразу повино бути дефолтне значення
+      background: formData.background // одразу повино бути дефолтне значення
+    })
+      .unwrap()
+      .then(response => {
+        console.log('Board created successfully:', response)
+      })
+      .catch(error => {
+        console.error('Error creating board:', error)
+      })
+  }
 
   const handleIconChange = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value)
   }
 
-  const handleCreateBoard = () => {
-    addNewBoardMutation({title:, icon: , background:})
-    .unwrap()
-      .then((response) => {
-        console.log('Доска успешно создана:', response)
-      })
-      .catch((error) => {
-        console.error('Ошибка при создании доски:', error)
-      })
-  }
-
-  
-
   return (
-    < size='sm' modalTitle='New board'>
+    <Modal size='sm' modalTitle='New board'>
       <Field
         {...register('title')}
         inputName='title'
         className={errors && 'mb-2'}
         placeholder='Title'
         errors={errors}
+        value={formData.title}
+        onChange={handleInputChange}
       />
       <p className='mt-6'>Icons</p>
       <Icons handleIconChange={handleIconChange} />
       <p className='mt-6'>Background</p>
       <BackgroundContainer />
-      <Button onClick={handleCreateBoard} disabled={isLoading}>
-        {isLoading ? 'Создание...' : 'Create'}
+      <Button isAddIcon iconName='plus' onClick={handleCreateBoard}>
+        Create
       </Button>
     </Modal>
   )
