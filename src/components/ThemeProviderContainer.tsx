62d9b13d5b9f67270fee293fae8983c80d68155f
Denys <babychdenya@gmail.com>
2024-02-08 16:21:35 +0000

    fix: removed array dependency

--- src/components/ThemeProviderContainer.tsx
+++ src/components/ThemeProviderContainer.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'next-themes'
 import type { PropsWithChildren } from 'react'
 import { useSelector } from 'react-redux'
-import { selectUser } from 'redux/slices/user/user-slice'
+import { selectTheme } from 'redux/slices/user/user-slice'
 
 export const ThemeProviderContainer = ({ children }: PropsWithChildren) => {
-  const user = useSelector(selectUser)
-
+  const theme = useSelector(selectTheme)
   return (
     <ThemeProvider
       attribute='class'
       themes={['light', 'dark', 'violet']}
-      defaultTheme={user.userTheme}>
+      defaultTheme={theme}>
       {children}
     </ThemeProvider>
   )
