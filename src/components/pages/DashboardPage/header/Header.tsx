e36198487d393e8269f2fbeac2039eb2f762fea0
Katya982 <xamko98@gmail.com>
2024-02-08 22:02:59 +0200

    feat: add burger menu

--- src/components/pages/DashboardPage/header/Header.tsx
+++ src/components/pages/DashboardPage/header/Header.tsx
@@ -1,13 +1,38 @@
-import { HeaderBurgerMenuBtn } from './HeaderBurgerMenuBtn'
-import { HeaderThemeSelect } from './HeaderThemeSelect'
-import { HeaderUserInfo } from './HeaderUserInfo'
+// import { HeaderBurgerMenuBtn } from './HeaderBurgerMenuBtn'
+// import { HeaderThemeSelect } from './HeaderThemeSelect'
+// import { HeaderUserInfo } from './HeaderUserInfo'
 
-export const Header = () => (
-  <header className='flex justify-between bg-white-primary px-5 py-[14px] dark:bg-black desktop:justify-end'>
-    <HeaderBurgerMenuBtn />
-    <div className='flex items-center gap-[14px]'>
-      <HeaderThemeSelect />
-      <HeaderUserInfo />
-    </div>
-  </header>
-)
+// export const Header = () => (
+//   <header className='flex justify-between bg-white-primary px-5 py-[14px] dark:bg-black desktop:justify-end'>
+//     <HeaderBurgerMenuBtn />
+//     <div className='flex items-center gap-[14px]'>
+//       <HeaderThemeSelect />
+//       <HeaderUserInfo />
+//     </div>
+//   </header>
+// )
+
+import { HeaderBurgerMenuBtn } from './HeaderBurgerMenuBtn';
+import { HeaderThemeSelect } from './HeaderThemeSelect';
+import { HeaderUserInfo } from './HeaderUserInfo';
+import PropTypes from 'prop-types';
+
+interface HeaderProps {
+  toggleSidebar: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const Header = ({ toggleSidebar }: HeaderProps) => {
+  return (
+    <header className='flex justify-between bg-white-primary px-5 py-[14px] dark:bg-black desktop:justify-end'>
+      <HeaderBurgerMenuBtn toggleSidebar={toggleSidebar} />
+      <div className='flex items-center gap-[14px]'>
+        <HeaderThemeSelect />
+        <HeaderUserInfo />
+      </div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  toggleSidebar: PropTypes.func.isRequired,
+};
--- src/components/pages/DashboardPage/sidebar/Sidebar.tsx
+++ src/components/pages/DashboardPage/sidebar/Sidebar.tsx
@@ -1,17 +1,45 @@
-import { SidebarBoardsList } from './SidebarBoardsList'
-import { SidebarCreateBoardBtn } from './SidebarCreateBoardBtn'
-import { SidebarLogoutBtn } from './SidebarLogoutBtn'
-import { SidebarNav } from './SidebarNav'
-import { SidebarUserSupportBtn } from './SidebarUserSupportBtn'
+// import { SidebarBoardsList } from './SidebarBoardsList'
+// import { SidebarCreateBoardBtn } from './SidebarCreateBoardBtn'
+// import { SidebarLogoutBtn } from './SidebarLogoutBtn'
+// import { SidebarNav } from './SidebarNav'
+// import { SidebarUserSupportBtn } from './SidebarUserSupportBtn'
 
-export const Sidebar = () => (
-  <div className='fixed left-0 top-0 z-50 hidden h-dvh desktop:block'>
-    <div className='flex h-dvh w-56 flex-grow flex-col bg-white p-3.5 violet:bg-brand-secondary dark:bg-black-secondary tablet:w-[260px] tablet:p-6'>
-      <SidebarNav />
-      <SidebarCreateBoardBtn />
-      <SidebarBoardsList />
-      <SidebarUserSupportBtn />
-      <SidebarLogoutBtn />
+// export const Sidebar = () => (
+//   <div className='fixed left-0 top-0 z-50 hidden h-dvh desktop:block'>
+//     <div className='flex h-dvh w-56 flex-grow flex-col bg-white p-3.5 violet:bg-brand-secondary dark:bg-black-secondary tablet:w-[260px] tablet:p-6'>
+//       <SidebarNav />
+//       <SidebarCreateBoardBtn />
+//       <SidebarBoardsList />
+//       <SidebarUserSupportBtn />
+//       <SidebarLogoutBtn />
+//     </div>
+//   </div>
+// )
+
+
+import { SidebarBoardsList } from './SidebarBoardsList';
+import { SidebarCreateBoardBtn } from './SidebarCreateBoardBtn';
+import { SidebarLogoutBtn } from './SidebarLogoutBtn';
+import { SidebarNav } from './SidebarNav';
+import { SidebarUserSupportBtn } from './SidebarUserSupportBtn';
+
+interface SidebarProps {
+  isSidebarOpen: boolean;
+  onCloseSideBar: () => void;
+}
+
+export const Sidebar = ({ isSidebarOpen, onCloseSideBar }: SidebarProps) => {
+  return (
+    <div className={`fixed left-0 top-0 z-50 ${isSidebarOpen ? 'block' : 'hidden'} h-screen desktop:block`}>
+      {isSidebarOpen && (
+        <div className='flex h-screen w-56 flex-grow flex-col bg-white p-3.5 violet:bg-brand-secondary dark:bg-black-secondary tablet:w-[260px] tablet:p-6' onClick={onCloseSideBar}>
+          <SidebarNav />
+          <SidebarCreateBoardBtn />
+          <SidebarBoardsList />
+          <SidebarUserSupportBtn />
+          <SidebarLogoutBtn />
+        </div>
+      )}
     </div>
-  </div>
-)
+  );
+};
