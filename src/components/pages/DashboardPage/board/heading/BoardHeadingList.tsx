74282069ec6ee65abe68db7094fdee22a47e6873
Denys <babychdenya@gmail.com>
2024-02-12 13:15:08 +0000

    refactor: add new functionality and added api calls for columns

--- src/components/pages/DashboardPage/board/heading/BoardHeadingList.tsx
+++ src/components/pages/DashboardPage/board/heading/BoardHeadingList.tsx
@@ -1,14 +1,13 @@
-import { BoardHeadingItem } from "./BoardHeadingItem"
+import { BoardHeadingItem } from './BoardHeadingItem'
 import { useSelector } from 'react-redux'
-import { selectColumns } from "redux/slices/board/board-slice"
-
+import { selectColumns } from 'redux/slices/board/board-slice'
 
 export const BoardHeadingList = () => {
   const columns = useSelector(selectColumns)
   return (
     <div className='mb-[14px] mt-[39px] flex gap-[34px] overflow-x-auto tablet:mt-[26px] desktop:mt-[10px]'>
       {columns.map(column => (
-        <BoardHeadingItem title={column.title} key={column._id} />
+        <BoardHeadingItem column={column} key={column._id} />
       ))}
     </div>
   )
--- src/components/pages/DashboardPage/modals/columnModal/AddColumnModal.tsx
+++ src/components/pages/DashboardPage/modals/columnModal/AddColumnModal.tsx
@@ -1,6 +1,8 @@
 import { Button, Field, Modal } from 'components/ui'
 import { useAddColumnForm } from 'hooks/useAddColumn'
 import type { ColumnModal } from 'lib/schemas/addComumn-shema'
+import { handleErrorToast, handleSuccessToast } from 'lib/toasts'
+import { useEffect } from 'react'
 import { useModal } from 'react-modal-state'
 import { useLocation } from 'react-router-dom'
 import { useAddNewColumnMutation } from 'redux/api/dashboard/column'
@@ -8,17 +10,29 @@ import { useAddNewColumnMutation } from 'redux/api/dashboard/column'
 export const AddColumnModal = () => {
   const location = useLocation()
   const { close } = useModal('add-column-modal')
-  const [addNewColumn] = useAddNewColumnMutation()
+  const [addNewColumn, { isSuccess, isError, error }] =
+    useAddNewColumnMutation()
   const { register, errors, handleSubmit } = useAddColumnForm()
 
   const submit = (data: ColumnModal) => {
-    console.log(data)
     const pathParts = location.pathname.split('/')
     const name = pathParts[pathParts.length - 1]
     addNewColumn({ boardName: name, body: data })
       .unwrap()
       .then(() => close())
   }
+
+  useEffect(() => {
+    if (isSuccess) {
+      handleSuccessToast(`Column added successfully!`)
+    }
+    if (isError && error && 'status' in error)
+      handleErrorToast(
+        error?.status === 409
+          ? 'User with this email already exists. Please try different email.'
+          : 'Something went wrong. Please try again.'
+      )
+  }, [isError, isSuccess])
 
   return (
     <Modal size='sm' modalTitle='Add column'>
