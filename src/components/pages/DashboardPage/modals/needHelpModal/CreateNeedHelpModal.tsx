6405b335e4b339773beddf1446fc6d1fd840b32d
Cajamarquina <martorella.ana@gmail.com>
2024-02-13 23:32:02 +0100

    fix: need-help-modal-fix-add-useNeedHelpMutation

--- src/components/pages/DashboardPage/modals/needHelpModal/CreateNeedHelpModal.tsx
+++ src/components/pages/DashboardPage/modals/needHelpModal/CreateNeedHelpModal.tsx
@@ -1,18 +1,20 @@
 import { Button, Field, Modal } from 'components/ui';
 import { useNeedHelp } from 'hooks/useNeedHelp.ts';
-import axios from 'axios';
+import { useNeedHelpMutation } from 'redux/api/dashboard/dashboard.ts';
 
 export const CreateNeedHelpModal = () => {
   const { register, errors, isValid } = useNeedHelp();
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+  const [createHelp, { isLoading, isError }] = useNeedHelpMutation();
+
+  const handleSubmit = async (e: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const comment = formData.get('comment');
+
     try {
-      await axios.post('api/dashboard/help', {
-        email: formData.get('email'),
-        comment: formData.get('comment')
-      });
+      await createHelp({ email, comment });
       console.log('Data sent successfully');
     } catch (error) {
       console.error('Error sending data:', error);
@@ -34,9 +36,10 @@ export const CreateNeedHelpModal = () => {
           placeholder='Comment'
           className='mb-[24px] h-[154px] w-full resize-none rounded-lg border border-brand border-opacity-40 bg-transparent px-[18px] py-[14px] text-fs-14-lh-1.28-fw-400 text-black outline-none placeholder:opacity-40 focus:border-opacity-100 violet:border-brand-secondary dark:text-white'
         />
-        <Button type='submit' disabled={!isValid}>
-          Send
+         <Button type='submit' disabled={!isValid || isLoading}>
+          {isLoading ? 'Sending...' : 'Send'}
         </Button>
+        {isError && <p>Error sending data</p>}
       </form>
     </Modal>
   );
