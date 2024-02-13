1dcb7d5e5b33c59713411cf5c2df224a978eedac
Valentun2 <pocomaxawrkt@gmail.com>
2024-02-13 23:44:09 +0200

    fix: update validate

--- src/components/pages/DashboardPage/modals/cardModal/addCardModal.tsx
+++ src/components/pages/DashboardPage/modals/cardModal/addCardModal.tsx
@@ -1,20 +1,20 @@
-import { valibotResolver } from '@hookform/resolvers/valibot'
-import { RadioPriority } from 'components/ui/field/RadioPriority'
 import { Button, Field, Modal } from 'components/ui/index'
 import {
   AddCardSchema,
   type AddCardSchemaFields
 } from 'lib/schemas/addCard-schema'
-import { handleErrorToast, handleSuccessToast } from 'lib/toasts'
+import { valibotResolver } from '@hookform/resolvers/valibot'
 import { useForm } from 'react-hook-form'
 import { useModal } from 'react-modal-state'
-import { useLocation } from 'react-router-dom'
+import { RadioPriority } from 'components/ui/field/RadioPriority'
+import { handleErrorToast, handleSuccessToast } from 'lib/toasts'
 import { useAddNewTaskMutation } from 'redux/api/dashboard/task'
 
 export const AddCardModal = () => {
-  const location = useLocation()
   const pathParts = location.pathname.split('/')
   const name = pathParts[pathParts.length - 1]
+
+
 
   const [addNewTask] = useAddNewTaskMutation()
   const {
@@ -30,12 +30,11 @@ export const AddCardModal = () => {
     }
   })
   const { close } = useModal('add-card-modal')
-
   const onSubmit = (data: AddCardSchemaFields) => {
     addNewTask({
       boardName: name,
       body: data,
-      columnId: localStorage.getItem('columnId')
+      columnId: JSON.parse(localStorage.getItem('idColumn') || '""')
     })
       .unwrap()
       .then(() => {
@@ -44,7 +43,7 @@ export const AddCardModal = () => {
         reset()
       })
       .catch(() => {
-        handleErrorToast('Error creating task')
+        handleErrorToast("Oops! Something went wrong. Our team is already solving this problem. Please stay with us.")
       })
     close()
     reset()
@@ -60,11 +59,17 @@ export const AddCardModal = () => {
           placeholder='Title'
           {...register('title')}
         />
-        <textarea
+     <div className='relative'>   <textarea
           placeholder='Description'
           {...register('description')}
           className='mb-[24px] h-[154px] w-full resize-none rounded-lg border border-brand border-opacity-40 bg-transparent px-[18px] py-[14px] text-fs-14-lh-1.28-fw-400 text-black outline-none placeholder:opacity-40 focus:border-opacity-100 violet:border-brand-secondary dark:text-white'
         />
+        {errors.description && (
+            <span className=' absolute left-0 top-[154px] text-red-600'>
+            Please enter at least 2 characters.
+            </span>
+          )}
+        </div>
         <p className='mb-[4px] select-none text-fs-12-lh-normal-fw-400 text-black/50 violet:text-black/50 dark:text-white/50'>
           Label color
         </p>
@@ -95,8 +100,14 @@ export const AddCardModal = () => {
         <p className='mb-[4px] select-none text-fs-12-lh-normal-fw-400 text-black/50 violet:text-black/50 dark:text-white/50'>
           Deadline
         </p>
-        <input type='date' className='mb-[40px]' {...register('deadline')} />
-
+        <div className='relative'>
+          <input type='date' className='mb-[40px] ' {...register('deadline')} />
+          {errors.deadline && (
+            <span className=' absolute left-0 top-5 text-red-600'>
+              Wrong date!
+            </span>
+          )}
+        </div>
         <Button isAddIcon iconName='plus' type='submit' disabled={!isValid}>
           Add
         </Button>
