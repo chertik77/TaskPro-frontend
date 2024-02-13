45d2973ceca4663ad40f5084de1e3d454163aa55
Cajamarquina <martorella.ana@gmail.com>
2024-02-13 23:15:48 +0100

    fix: fix-need-help-modal

--- src/components/pages/DashboardPage/modals/needHelpModal/CreateNeedHelpModal.tsx
+++ src/components/pages/DashboardPage/modals/needHelpModal/CreateNeedHelpModal.tsx
@@ -1,28 +1,43 @@
-import { Button, Field, Modal } from 'components/ui'
-import { useNeedHelp } from 'hooks/useNeedHelp.ts'
+import { Button, Field, Modal } from 'components/ui';
+import { useNeedHelp } from 'hooks/useNeedHelp.ts';
+import axios from 'axios';
 
 export const CreateNeedHelpModal = () => {
-  const { register, errors } = useNeedHelp()
+  const { register, errors, isValid } = useNeedHelp();
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    try {
+      await axios.post('api/dashboard/help', {
+        email: formData.get('email'),
+        comment: formData.get('comment')
+      });
+      console.log('Data sent successfully');
+    } catch (error) {
+      console.error('Error sending data:', error);
+    }
+  };
 
   return (
     <Modal size='sm' modalTitle='Need help'>
-      <Field
-        {...register('email')}
-        inputName='Email'
-        placeholder='Email address'
-        errors={errors}
-        className='mb-6'
-      />
- <Field
-        {...register('comment')}
-        inputName='Comment'
-        placeholder='Comment'
-        errors={errors}
-        className='mb-6'
-      />
-      <Button onClick={e => console.log(e)}>
-        Send
-      </Button>
+      <form onSubmit={handleSubmit}>
+        <Field
+          {...register('email')}
+          inputName='Email'
+          placeholder='Email address'
+          errors={errors}
+          className='mb-6'
+        />
+        <textarea
+          {...register('comment')}
+          placeholder='Comment'
+          className='mb-[24px] h-[154px] w-full resize-none rounded-lg border border-brand border-opacity-40 bg-transparent px-[18px] py-[14px] text-fs-14-lh-1.28-fw-400 text-black outline-none placeholder:opacity-40 focus:border-opacity-100 violet:border-brand-secondary dark:text-white'
+        />
+        <Button type='submit' disabled={!isValid}>
+          Send
+        </Button>
+      </form>
     </Modal>
-  )
-}
+  );
+};
--- src/hooks/useNeedHelp.ts
+++ src/hooks/useNeedHelp.ts
@@ -2,14 +2,15 @@ import { valibotResolver } from '@hookform/resolvers/valibot';
 import { NeedHelpModal, needHelpSchema } from '../lib/schemas/needHelpModal-schema';
 import { useForm } from 'react-hook-form';
 
+
 export const useNeedHelp = () => {
   const {
     register,
-    formState: { errors }
+    formState: { errors, isValid }
   } = useForm<NeedHelpModal>({
     resolver: valibotResolver(needHelpSchema),
     mode: 'onChange'
   });
 
-  return { register, errors };
+  return { register, errors, isValid };
 };
