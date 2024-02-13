f872a78653a614f83b9601018ce7877e254032be
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2024-02-13 03:30:36 +0200

    fix: fix conflict

--- src/components/pages/DashboardPage/sidebar/SidebarLogoutBtn.tsx
+++ src/components/pages/DashboardPage/sidebar/SidebarLogoutBtn.tsx
@@ -5,7 +5,6 @@ import { useLogoutMutation } from 'redux/api/user'
 export const SidebarLogoutBtn = () => {
   const [logout] = useLogoutMutation()
   const { close } = useModal('burger-menu');
-
   
   const handleLogout = () => {
     logout
@@ -14,9 +13,8 @@ export const SidebarLogoutBtn = () => {
   
   return (
     <Button className=' my-6 ml-6 flex items-center gap-3.5'
-    // onClick={logout} 
-onClick={handleLogout}
-    >
+      onClick={handleLogout}>
+      
       <svg className='size-8 text-brand hocus:text-brand-hover violet:text-white violet:hocus:text-brand-third'>
         <use xlinkHref='/assets/icons.svg#icon-logout-btn' />
       </svg>
