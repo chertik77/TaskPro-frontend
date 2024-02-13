ddc096de2fe0a670631dfddc7d7ec0d5dc1146f2
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2024-02-14 01:48:17 +0200

    fix: fixed let

--- src/lib/schemas/addCard-schema.ts
+++ src/lib/schemas/addCard-schema.ts
@@ -9,15 +9,10 @@ import {
 } from 'valibot'
 
 const priorityList = ['Low', 'Medium', 'High', 'Without priority']
-const today = new Date();
-const yyyy = today.getFullYear();
-let mm = today.getMonth() + 1; // Months start at 0!
-let dd = today.getDate();
-
-
-
-
-
+const today = new Date()
+const yyyy = today.getFullYear()
+const mm = today.getMonth() + 1 // Months start at 0!
+const dd = today.getDate()
 
 export const AddCardSchema = object({
   title: string([
@@ -29,6 +24,8 @@ export const AddCardSchema = object({
     minLength(2, 'Please enter at least 2 characters.')
   ]),
   priority: picklist(priorityList),
-  deadline: string([minValue(`${yyyy}-${mm.toString().padStart(2, '0')}-${dd}`)])
+  deadline: string([
+    minValue(`${yyyy}-${mm.toString().padStart(2, '0')}-${dd}`)
+  ])
 })
 export type AddCardSchemaFields = Output<typeof AddCardSchema>
