e811a371e8929c4c0211f2bdee1fc5096bc626ec
MorskoySom <Somovvitvit@gmail.com>
2024-02-14 17:18:35 +0200

    feat: add_colors_to_scroll

--- src/components/pages/dashboard/board/Board.tsx
+++ src/components/pages/dashboard/board/Board.tsx
@@ -33,11 +33,11 @@ export const Board = () => {
       {boardData !== null && (
         <div className='col-start-2 row-start-2 flex flex-col gap-[39px] overflow-x-auto px-[20px] pt-[14px] text-fs-14-lh-normal-fw-500 text-black dark:text-white tablet:gap-[26px] tablet:pl-[32px] tablet:pt-[26px] tablet:text-fs-18-lh-normal-fw-500  desktop:gap-[10px] desktop:pl-[24px] desktop:pt-[10px]'>
           {boardData?.title}
-          <div className='flex'>
+          <div className='flex h-screen overflow-x-auto scrollbar scrollbar-track-white  scrollbar-thumb-scroll-track1 violet:scrollbar-thumb-brand-third dark:scrollbar-track-black dark:scrollbar-thumb-scroll-thumb'>
             <BoardHeadingList />
             <Button
               onClick={open}
-              className='mb-[14px] mt-[39px] inline-flex h-[56px] w-[100%] items-center justify-center gap-2 rounded-md bg-white py-[14px] text-fs-14-lh-normal-fw-500 dark:bg-black-secondary mobile:min-w-[335px] mobile:max-w-[335px] mobile:px-[79px] tablet:mt-[26px] tablet:min-w-[334px] tablet:max-w-[334px] tablet:pl-[79px] tablet:pr-[78px] desktop:mt-[10px]'>
+              className='mb-[14px] mt-[39px] inline-flex h-[56px] w-[100%] items-center justify-center gap-2 rounded-md bg-white py-[14px] text-fs-14-lh-normal-fw-500 violet:bg-white violet:text-black dark:bg-black-secondary mobile:min-w-[335px] mobile:max-w-[335px] mobile:px-[79px] tablet:mt-[26px] tablet:min-w-[334px] tablet:max-w-[334px] tablet:pl-[79px] tablet:pr-[78px] desktop:mt-[10px]'>
               <svg className='size-7 rounded-md bg-black text-white violet:bg-brand-secondary violet:text-white dark:bg-white dark:text-black'>
                 <use xlinkHref='/assets/icons.svg#icon-pluss' />
               </svg>
