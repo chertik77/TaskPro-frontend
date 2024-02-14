4faae49a703c78a719c943a7ea6e0a1333a2df60
Valeriia Trytiak <valeriia.trytiak@gmail.com>
2024-02-14 10:44:10 +0100

    style: fix height sidebar

--- src/components/pages/DashboardPage/sidebar/Sidebar.tsx
+++ src/components/pages/DashboardPage/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { SidebarNav } from './SidebarNav'
 import { SidebarUserSupportBtn } from './SidebarUserSupportBtn'
 
 export const Sidebar = () => (
-  <div className='col-start-1 row-span-2 row-start-1 hidden h-dvh overflow-y-auto overflow-x-hidden desktop:block'>
+  <div className='col-start-1 row-span-2 row-start-1 hidden min-h-dvh overflow-y-auto overflow-x-hidden desktop:block'>
     <div className='flex min-h-full w-56 flex-grow flex-col bg-white bg-local violet:bg-brand-secondary dark:bg-black-secondary tablet:w-[260px]'>
       <SidebarNav />
       <SidebarCreateBoardBtn />
