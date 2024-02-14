0b24efafa428f60b4b97d0fec3140e46228794e0
Denys <babychdenya@gmail.com>
2024-02-14 02:57:25 +0000

    chore: resolve conflicts

--- src/components/pages/dashboard/modals/index.ts
+++ src/components/pages/dashboard/modals/index.ts
@@ -1,7 +1,6 @@
 export * from './EditProfileModal/EditProfileModal'
 export * from './boardModal/EditBoardModal'
 export * from './boardModal/NewBoardModal'
-export * from './burgerMenu/BurgerMenu'
 export * from './cardModal/addCardModal'
 export * from './cardModal/editCardModal'
 export * from './columnModal/AddColumnModal'
