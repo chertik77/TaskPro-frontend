8b48268a0ec2d7e3efb33942c32a6aba7e7d8658
chertik77 <denysbabych.dev@gmail.com>
2024-05-23 16:39:39 +0100

    fix: remove chrome scroll when board is empty

--- src/components/ui/CreateBoard.tsx
+++ src/components/ui/CreateBoard.tsx
@@ -6,18 +6,20 @@ export const CreateBoard = () => {
   const { open } = useModal(AddBoardModal)
 
   return (
-    <div className='flex items-center justify-center'>
+    <div
+      className='max-tablet:absolute max-tablet:left-1/2 max-tablet:top-1/2
+        max-tablet:-translate-x-1/2 max-tablet:-translate-y-1/2 desktop:flex
+        desktop:items-center desktop:justify-center'>
       <div
-        className='w-[335px] text-center text-fs-12-lh-1.33-fw-400 text-black opacity-70
-          dark:text-white-gray-secondary tablet:h-[72px] tablet:w-[486px]
-          tablet:text-fs-14-lh-1.28-fw-400 max-tablet:px-5'>
+        className='w-[335px] px-5 text-center text-fs-12-lh-1.33-fw-400 text-black/70
+          dark:text-white-gray-secondary tablet:w-[486px] tablet:text-fs-14-lh-1.28-fw-400'>
         Before starting your project, it is essential{' '}
-        <span
+        <button
           onClick={open}
-          className='text-brand hocus:cursor-pointer hocus:text-brand-hover
-            violet:text-brand-secondary violet:hocus:text-brand-third'>
-          to create a board{' '}
-        </span>
+          className='text-brand hocus:text-brand-hover violet:text-brand-secondary
+            violet:hocus:text-brand-third'>
+          to create a board
+        </button>{' '}
         to visualize and track all the necessary tasks and milestones. This
         board serves as a powerful tool to organize the workflow and ensure
         effective collaboration among team members.
