3fde222aa199a69ab525f33b4a9b1b9e493aef3b
chertik77 <denysbabych.dev@gmail.com>
2024-05-23 20:17:24 +0100

    feat: move toast on top-center on laptop and mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,18 @@
 import { useTheme } from 'next-themes'
+import { useMediaQuery } from 'react-responsive'
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 export const Layout = () => {
   const { theme } = useTheme()
 
+  const isLaptopOrMobile = useMediaQuery({ maxWidth: 1440 })
+
   return (
     <>
       <Outlet />
       <Toaster
+        position={isLaptopOrMobile ? 'top-center' : 'bottom-right'}
         richColors
         theme={theme === 'dark' ? 'dark' : 'light'}
         className='text-balance'
