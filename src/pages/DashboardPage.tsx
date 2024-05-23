de6e58ffd1ad08559d24086fe2d3d3fc0ed9a535
chertik77 <denysbabych.dev@gmail.com>
2024-05-23 16:02:34 +0100

    feat: add custom scroll for board, and overflow to disable scroll on ios

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,8 +4,8 @@ import { Header, Sidebar } from 'components/dashboard'
 
 const DashboardPage = () => (
   <div
-    className='mobile:grid-cols-[225px,1fr] desktop:grid desktop:h-dvh
-      desktop:grid-cols-[260px,1fr] desktop:grid-rows-[auto,1fr]'>
+    className='grid-cols-[225px,1fr] desktop:grid desktop:grid-cols-[260px,1fr]
+      desktop:grid-rows-[auto,1fr]'>
     <Sidebar />
     <Header />
     <Outlet />
