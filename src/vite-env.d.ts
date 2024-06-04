6b2be065d0d6d4f7534f15ae7fede76c6973c9f2
chertik77 <denysbabych.dev@gmail.com>
2024-06-04 14:49:10 +0100

    chore: add types for env files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
