6e49e8ae862678cc63a5dc1f213a5d60e9f5392a
chertik77 <denysbabych.dev@gmail.com>
2024-06-04 15:11:38 +0100

    refactor: update react-router structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import type { AxiosError } from 'axios'
 
 import { StrictMode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SidebarProvider } from 'contexts/sidebar.context'
 import { ThemeProvider } from 'contexts/theme.context'
+import { DashboardPage, HomePage, SigninPage, SignupPage } from 'pages'
 import ReactDOM from 'react-dom/client'
 import { ModalProvider, ModalRenderer } from 'react-modal-state'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 
-import { App } from 'components/App'
+import { Board } from 'components/dashboard'
 import {
   AddCardModal,
   BurgerMenu,
@@ -19,6 +21,9 @@ import {
   NeedHelpModal,
   NewBoardModal
 } from 'components/dashboard/modals'
+import { Layout } from 'components/Layout'
+import { PrivateRoute, RestrictedRoute } from 'components/routes'
+import { CreateBoard } from 'components/ui'
 
 import { persistor, store } from 'redux/store'
 
@@ -36,6 +41,40 @@ declare module '@tanstack/react-query' {
   }
 }
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <RestrictedRoute component={<HomePage />} /> }
+    ]
+  },
+  {
+    path: '/auth/signin',
+    element: <RestrictedRoute component={<SigninPage />} />
+  },
+  {
+    path: '/auth/signup',
+    element: <RestrictedRoute component={<SignupPage />} />
+  },
+  {
+    path: '/dashboard',
+    element: (
+      <PrivateRoute
+        component={
+          <SidebarProvider>
+            <DashboardPage />
+          </SidebarProvider>
+        }
+      />
+    ),
+    children: [
+      { index: true, element: <CreateBoard /> },
+      { path: ':boardId', element: <Board /> }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}> */}
@@ -44,20 +83,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <PersistGate
           loading={null}
           persistor={persistor}>
-          <BrowserRouter>
-            <ThemeProvider>
-              <ModalProvider>
-                <App />
-                <ModalRenderer Component={EditBoardModal} />
-                <ModalRenderer Component={NewBoardModal} />
-                <ModalRenderer Component={NeedHelpModal} />
-                <ModalRenderer Component={AddCardModal} />
-                <ModalRenderer Component={EditCardModal} />
-                <ModalRenderer Component={EditProfileModal} />
-                <ModalRenderer Component={BurgerMenu} />
-              </ModalProvider>
-            </ThemeProvider>
-          </BrowserRouter>
+          <ThemeProvider>
+            <ModalProvider>
+              <RouterProvider router={router} />
+              <ModalRenderer Component={EditBoardModal} />
+              <ModalRenderer Component={NewBoardModal} />
+              <ModalRenderer Component={NeedHelpModal} />
+              <ModalRenderer Component={AddCardModal} />
+              <ModalRenderer Component={EditCardModal} />
+              <ModalRenderer Component={EditProfileModal} />
+              <ModalRenderer Component={BurgerMenu} />
+            </ModalProvider>
+          </ThemeProvider>
         </PersistGate>
       </Provider>
     </QueryClientProvider>
