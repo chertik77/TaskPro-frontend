4f426f3e192badbe022934092c9c8343ebf42838
chertik77 <denysbabych.dev@gmail.com>
2024-06-04 21:14:19 +0100

    feat: add pages-url config

--- src/components/routes/PrivateRoute.tsx
+++ src/components/routes/PrivateRoute.tsx
@@ -5,10 +5,12 @@ import { Navigate } from 'react-router-dom'
 
 import { selectIsLoggedIn } from 'redux/user.slice'
 
+import { Pages } from 'config'
+
 export const PrivateRoute = ({ component }: { component: ReactElement }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn)
 
   const shouldRedirect = !isLoggedIn
 
-  return shouldRedirect ? <Navigate to='/' /> : component
+  return shouldRedirect ? <Navigate to={Pages.Home} /> : component
 }
