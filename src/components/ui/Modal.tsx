15a5ffedf4992e0eb3c71ab6dd420298f442565d
chertik77 <denysbabych.dev@gmail.com>
2024-06-09 02:45:46 +0100

    style: add adaptive style on need-help modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -26,7 +26,7 @@ export const Modal = ({ children, modalTitle, ...props }: DialogProps) => {
       onClose={close}
       center
       classNames={{
-        modal: `${cn('shadow-none m-0 w-8xl rounded-lg p-6 dark:bg-black tablet:w-[350px]', modalTitle === 'Need help' && 'w-[400px]')}`,
+        modal: `${cn('shadow-none m-0 w-8xl rounded-lg p-6 dark:bg-black tablet:w-[350px]', modalTitle === 'Need help' && 'max-tablet:w-8xl')}`,
         closeIcon: 'dark:fill-white size-lg',
         overlay: 'backdrop-saturate-150 backdrop-blur-md'
       }}
