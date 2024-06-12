08059c26d3461e708b8c9d8746c1824850cc38c5
chertik77 <denysbabych.dev@gmail.com>
2024-06-12 11:55:33 +0100

    fix: minor bug fixes

--- src/components/dashboard/board/columns/BoardColumnsItem.tsx
+++ src/components/dashboard/board/columns/BoardColumnsItem.tsx
@@ -1,6 +1,7 @@
 import type { Column } from 'types'
 
 import { useSelector } from 'react-redux'
+import { useMediaQuery } from 'react-responsive'
 
 import { selectCardPriority, selectCardSortCriterion } from 'redux/filter.slice'
 
@@ -17,26 +18,26 @@ export const BoardColumnsItem = ({ column }: { column: Column }) => {
 
   const sortedCards = getSortedCards(filteredCards, cardSortCriterion)
 
+  const isTabletOrMobile = useMediaQuery({ minWidth: 320, maxWidth: 1439 })
+
   return (
     <>
       <BoardColumnsActions
         column={column}
         sortedCardsLength={sortedCards?.length}
       />
-      {sortedCards && sortedCards?.length > 0 && (
-        <div
-          className={cn(
-            'custom-scrollbar -mr-4 space-y-2 overflow-y-auto pr-4',
-            !cardPriority && 'h-[calc(100dvh-270px)]'
-          )}>
-          {sortedCards.map(card => (
-            <BoardCard
-              card={card}
-              key={card.id}
-            />
-          ))}
-        </div>
-      )}
+      <div
+        className={cn('custom-scrollbar -mr-4 space-y-2 overflow-y-auto pr-4', {
+          'h-[calc(100dvh-270px)]': !cardPriority && !isTabletOrMobile,
+          'h-[calc(100dvh-300px)]': !cardPriority && isTabletOrMobile
+        })}>
+        {sortedCards?.map(card => (
+          <BoardCard
+            card={card}
+            key={card.id}
+          />
+        ))}
+      </div>
     </>
   )
 }
