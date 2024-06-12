719284c4240dcf4cbf1c574a549336d280878c14
chertik77 <denysbabych.dev@gmail.com>
2024-06-12 12:52:58 +0100

    fix: minor bug fixes

--- src/contexts/sidebar.context.tsx
+++ src/contexts/sidebar.context.tsx
@@ -2,6 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 import { createContext, useContext, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
+import { useMediaQuery } from 'react-responsive'
 
 type SidebarProviderState = {
   isSidebarOpen: boolean
@@ -18,8 +19,11 @@ const SidebarProviderContext = createContext<SidebarProviderState>(initialState)
 export const SidebarProvider = ({ children }: PropsWithChildren) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
 
+  const isTabletAndBelow = useMediaQuery({ maxWidth: 1439 })
+
   useHotkeys('mod+o', () => setIsSidebarOpen(prev => !prev), {
-    preventDefault: true
+    preventDefault: true,
+    ignoreEventWhen: () => isTabletAndBelow
   })
 
   const value = {
