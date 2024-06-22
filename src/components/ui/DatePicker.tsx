05f951c96e4b4adc29da24e0bc408c6711b747e1
chertik77 <denysbabych.dev@gmail.com>
2024-06-22 18:02:27 +0100

    fix: close calendar popover when day has been selected

--- src/components/ui/DatePicker.tsx
+++ src/components/ui/DatePicker.tsx
@@ -1,6 +1,7 @@
 import type { CardSchema } from 'lib/schemas'
 import type { Control } from 'react-hook-form'
 
+import { useState } from 'react'
 import { Content, Popover, Portal, Trigger } from '@radix-ui/react-popover'
 import { Controller } from 'react-hook-form'
 import { MdKeyboardArrowDown } from 'react-icons/md'
@@ -13,32 +14,42 @@ type DatePickerProps = {
   control: Control<CardSchema>
 }
 
-export const DatePicker = ({ control }: DatePickerProps) => (
-  <div className='relative'>
-    <p className='mb-1 text-sm text-black/50 dark:text-white/50'>Deadline</p>
-    <Controller
-      control={control}
-      name='deadline'
-      render={({ field }) => (
-        <Popover>
-          <Trigger className='mb-[40px] flex items-center gap-1 text-brand violet:text-brand-secondary'>
-            {formatTodayDate(field.value)}
-            <MdKeyboardArrowDown className='size-lg text-current' />
-          </Trigger>
-          <Portal>
-            <Content
-              side='top'
-              className='z-[1000]'
-              align='start'>
-              <Calendar
-                mode='single'
-                selected={field.value}
-                onSelect={field.onChange}
-              />
-            </Content>
-          </Portal>
-        </Popover>
-      )}
-    />
-  </div>
-)
+export const DatePicker = ({ control }: DatePickerProps) => {
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false)
+
+  return (
+    <div className='relative'>
+      <p className='mb-1 text-sm text-black/50 dark:text-white/50'>Deadline</p>
+      <Controller
+        control={control}
+        name='deadline'
+        render={({ field }) => (
+          <Popover
+            open={isCalendarOpen}
+            onOpenChange={setIsCalendarOpen}>
+            <Trigger className='mb-[40px] flex items-center gap-1 text-brand violet:text-brand-secondary'>
+              {formatTodayDate(field.value)}
+              <MdKeyboardArrowDown className='size-lg text-current' />
+            </Trigger>
+            <Portal>
+              <Content
+                side='top'
+                sideOffset={5}
+                className='z-[1000]'
+                align='start'>
+                <Calendar
+                  mode='single'
+                  selected={field.value}
+                  onSelect={date => {
+                    field.onChange(date)
+                    setIsCalendarOpen(false)
+                  }}
+                />
+              </Content>
+            </Portal>
+          </Popover>
+        )}
+      />
+    </div>
+  )
+}
