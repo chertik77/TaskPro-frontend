54748703ff063ad8df417de9c1a71261f5d7cf7d
chertik77 <denysbabych.dev@gmail.com>
2024-07-12 23:04:54 +0100

    fix: removed overflow from column item

--- src/components/dashboard/board/columns/BoardColumnsItem.tsx
+++ src/components/dashboard/board/columns/BoardColumnsItem.tsx
@@ -37,11 +37,11 @@ export const BoardColumnsItem = ({
       <BoardColumnsActions column={column} />
       <ScrollArea.Root
         type='scroll'
-        className={cn('-mr-4 overflow-y-hidden pr-4', {
+        className={cn('-mr-4 pr-4', {
           'h-[calc(100dvh-275px)]': !isTabletAndBelow,
           'h-[calc(100dvh-300px)]': isTabletAndBelow
         })}>
-        <ScrollArea.Viewport className='h-full !overflow-x-visible'>
+        <ScrollArea.Viewport className='h-full'>
           {sortedCards?.map(card => (
             <BoardCard
               card={card}
