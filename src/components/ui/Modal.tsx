499a5f2a876c6d909fb8e6f433f52fa1e42022ac
chertik77 <denysbabych.dev@gmail.com>
2024-08-22 06:55:36 +0100

    fix: add focus trap

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react'
 import type { ModalProps } from 'react-responsive-modal'
 
+import FocusTrap from 'focus-trap-react'
 import { useModalInstance } from 'react-modal-state'
 import { Modal as Dialog } from 'react-responsive-modal'
 
@@ -21,18 +22,21 @@ export const Modal = ({ children, modalTitle, ...props }: DialogProps) => {
   const { isOpen, close } = useModalInstance()
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={close}
-      center
-      classNames={{
-        modal: `${cn('shadow-none m-0 w-8xl rounded-lg p-6 dark:bg-black tablet:w-[350px]', modalTitle === 'Need help' && 'max-tablet:w-8xl')}`,
-        closeIcon: 'dark:fill-white size-lg',
-        overlay: 'backdrop-saturate-150 backdrop-blur-md'
-      }}
-      {...props}>
-      <h4 className='mb-6 text-lg'>{modalTitle}</h4>
-      {children}
-    </Dialog>
+    <FocusTrap>
+      <Dialog
+        open={isOpen}
+        focusTrapped={false}
+        onClose={close}
+        center
+        classNames={{
+          modal: `${cn('shadow-none m-0 w-8xl rounded-lg p-6 dark:bg-black tablet:w-[350px]', modalTitle === 'Need help' && 'max-tablet:w-8xl')}`,
+          closeIcon: 'dark:fill-white size-lg',
+          overlay: 'backdrop-saturate-150 backdrop-blur-md'
+        }}
+        {...props}>
+        <h4 className='mb-6 text-lg'>{modalTitle}</h4>
+        {children}
+      </Dialog>
+    </FocusTrap>
   )
 }
