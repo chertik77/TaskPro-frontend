f6851903a6fda394256151d36ea39c092bce6df2
chertik77 <denysbabych.dev@gmail.com>
2024-11-02 17:37:55 +0000

    feat: add vercel analytics and speed insights

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { Toaster } from 'sonner'
@@ -14,6 +16,8 @@ export const Layout = () => {
   return (
     <>
       <Outlet />
+      <Analytics />
+      <SpeedInsights />
       <Toaster
         position={isTabletAndBelow ? 'top-center' : 'bottom-right'}
         richColors
