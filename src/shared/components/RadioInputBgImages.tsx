0f0473661468c304634a977621b87ca170e94459
chertik77 <denysbabych.dev@gmail.com>
2025-02-16 22:48:24 +0000

    style: add loading for board bg images

--- src/shared/components/RadioInputBgImages.tsx
+++ src/shared/components/RadioInputBgImages.tsx
@@ -8,6 +8,8 @@ import { Controller } from 'react-hook-form'
 
 import { useAuthStore } from '@/shared/store'
 
+import { cn } from '../lib'
+
 type BoardImages = {
   id: string
   icon: string | Record<UserTypes.Theme, string>
@@ -20,7 +22,7 @@ type RadioInputBgImagesProps = {
 export const RadioInputBgImages = ({ control }: RadioInputBgImagesProps) => {
   const theme = useAuthStore(state => state.user.theme)
 
-  const { data: boardImages } = useQuery<BoardImages>({
+  const { data: boardImages, isPending } = useQuery<BoardImages>({
     queryKey: ['board-bg-images'],
     queryFn: async () =>
       await fetch('/board-bg-images.json').then(res => res.json())
@@ -34,23 +36,32 @@ export const RadioInputBgImages = ({ control }: RadioInputBgImagesProps) => {
         name='background'
         render={({ field }) => (
           <Root
-            className='mb-10 mt-3.5 flex max-w-[280px] flex-wrap gap-2'
+            className={cn(
+              'mb-10 mt-3.5 flex max-w-[280px] flex-wrap gap-2',
+              isPending && 'mt-4'
+            )}
             onValueChange={field.onChange}>
-            {boardImages?.map(({ id, icon }) => (
-              <Item
-                checked={field.value === id}
-                value={id}
-                className='focus-visible:styled-outline group outline-offset-4'
-                key={id}>
-                <img
-                  className='group-aria-checked:scale-125'
-                  width={28}
-                  height={28}
-                  src={typeof icon === 'object' ? icon[theme] : icon}
-                  alt={id}
-                />
-              </Item>
-            ))}
+            {isPending ? (
+              <p className='text-gray-secondary'>
+                Loading board backgrounds...
+              </p>
+            ) : (
+              boardImages?.map(({ id, icon }) => (
+                <Item
+                  checked={field.value === id}
+                  value={id}
+                  className='focus-visible:styled-outline group outline-offset-4'
+                  key={id}>
+                  <img
+                    className='group-aria-checked:scale-125'
+                    width={28}
+                    height={28}
+                    src={typeof icon === 'object' ? icon[theme] : icon}
+                    alt={id}
+                  />
+                </Item>
+              ))
+            )}
           </Root>
         )}
       />
