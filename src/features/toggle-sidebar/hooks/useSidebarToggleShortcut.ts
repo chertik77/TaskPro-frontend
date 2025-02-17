397e3700a510707a55b0910432ae4c2333a35a99
chertik77 <denysbabych.dev@gmail.com>
2025-02-17 14:12:10 +0000

    refactor: remove react-hotkeys package, add keyboard shortcut for sidebar toggle

--- src/features/toggle-sidebar/hooks/useSidebarToggleShortcut.ts
+++ src/features/toggle-sidebar/hooks/useSidebarToggleShortcut.ts
@@ -1,15 +1,29 @@
-import { useHotkeys } from 'react-hotkeys-hook'
+import { useEffect } from 'react'
 
 import { useTabletAndBelowMediaQuery } from '@/shared/hooks'
 import { useSidebarStore } from '@/shared/store'
+
+const SIDEBAR_KEYBOARD_SHORTCUT = 'o'
 
 export const useSidebarToggleShortcut = () => {
   const { isSidebarOpen, setIsSidebarOpen } = useSidebarStore()
 
   const isTabletAndBelow = useTabletAndBelowMediaQuery()
 
-  useHotkeys('mod+o', () => setIsSidebarOpen(!isSidebarOpen), {
-    preventDefault: true,
-    ignoreEventWhen: () => isTabletAndBelow
-  })
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (
+        e.key === SIDEBAR_KEYBOARD_SHORTCUT &&
+        (e.metaKey || e.ctrlKey) &&
+        !isTabletAndBelow
+      ) {
+        e.preventDefault()
+        setIsSidebarOpen(!isSidebarOpen)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [isSidebarOpen, isTabletAndBelow, setIsSidebarOpen])
 }
