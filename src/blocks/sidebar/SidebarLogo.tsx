ba8a7b83764fb6ac5082eafffb30afefba1c4cf8
chertik77 <denysbabych.dev@gmail.com>
2025-05-17 17:26:20 +0100

    fix: minor updates & improvements

--- src/blocks/sidebar/SidebarLogo.tsx
+++ src/blocks/sidebar/SidebarLogo.tsx
@@ -9,18 +9,20 @@ export const SidebarLogo = () => {
   )
 
   return (
-    <Link
-      className='focus-visible:styled-outline violet:text-brand-violet tablet:mb-[60px]
-        tablet:pl-6 desktop:pt-6 mb-[70px] flex items-center gap-2 pl-3.5 text-white'
-      to='/dashboard'
-      onClick={() => toggleSidebarMobileMenu(false)}>
-      <Icon
-        name='logo'
-        className='fill-black-muted violet:fill-white-gray size-8'
-      />
-      <h1 className='violet:text-white text-lg font-semibold text-black dark:text-white'>
-        Task Pro
-      </h1>
-    </Link>
+    <div className='tablet:mb-[60px] tablet:pl-6 desktop:pt-6 mb-[70px] pl-3.5'>
+      <Link
+        className='focus-visible:styled-outline violet:text-brand-violet inline-flex items-center
+          gap-2 text-white'
+        to='/dashboard'
+        onClick={() => toggleSidebarMobileMenu(false)}>
+        <Icon
+          name='logo'
+          className='fill-black-muted violet:fill-white-gray size-8'
+        />
+        <h1 className='violet:text-white text-lg font-semibold text-black dark:text-white'>
+          Task Pro
+        </h1>
+      </Link>
+    </div>
   )
 }
