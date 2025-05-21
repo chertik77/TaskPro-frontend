e9d612370866f43385cbdae493c3ded99a40a820
chertik77 <denysbabych.dev@gmail.com>
2025-05-21 15:35:41 +0100

    fix: minor updates & improvements

--- src/features/card/move-card/components/MoveCardSelect.tsx
+++ src/features/card/move-card/components/MoveCardSelect.tsx
@@ -52,7 +52,7 @@ export const MoveCardSelect = memo(
                 key={column.id}
                 className='group flex items-center gap-2'
                 value={column.id}>
-                <p className='group-focus:stroke-brand violet:group-focus:stroke-brand-violet w-20 truncate'>
+                <p className='group-focus:text-brand violet:group-focus:text-brand-violet w-20 truncate'>
                   {column.title}
                 </p>
                 <Icon
