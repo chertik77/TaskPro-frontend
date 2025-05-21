9b2efefce9b69b882b36cd7f99d952212ebf1565
chertik77 <denysbabych.dev@gmail.com>
2025-05-21 20:11:05 +0100

    fix: minor updates & improvements

--- src/features/card/filter-cards/utils/getFilteredCards.ts
+++ src/features/card/filter-cards/utils/getFilteredCards.ts
@@ -37,7 +37,7 @@ export const getFilteredCards = (
 
       if (deadline === 'Upcoming') {
         return isWithinInterval(cardDeadline, {
-          start: startOfToday(),
+          start: today,
           end: nextWeek
         })
       }
