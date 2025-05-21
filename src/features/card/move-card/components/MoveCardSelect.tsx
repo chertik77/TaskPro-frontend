4f4cf8ee26cf04529227d13e9788a5380628e247
chertik77 <denysbabych.dev@gmail.com>
2025-05-21 13:28:44 +0100

    feat: add reusable select ui component

--- src/features/card/move-card/components/MoveCardSelect.tsx
+++ src/features/card/move-card/components/MoveCardSelect.tsx
@@ -1,8 +1,14 @@
 import { memo, useMemo } from 'react'
-import * as Select from '@radix-ui/react-select'
 
 import { useGetBoardById } from '@/shared/hooks'
-import { Icon } from '@/shared/ui'
+import {
+  Icon,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectViewport
+} from '@/shared/ui'
 
 import { useMoveCard } from '../hooks/useMoveCard'
 
@@ -26,43 +32,37 @@ export const MoveCardSelect = memo(
     const { mutate: moveCard } = useMoveCard()
 
     return (
-      <Select.Root
+      <Select
         value={cardColumnId}
         onValueChange={v => moveCard({ cardId: cardId, newColumnId: v })}
         disabled={columns && columns.length <= 1}>
-        <Select.Trigger className='group focus-visible:styled-outline disabled:hidden'>
+        <SelectTrigger className='group disabled:hidden'>
           <Icon
             name='arrow-circle'
             className='group-hocus:stroke-black dark:group-hocus:stroke-white size-4 stroke-black/50
               dark:stroke-white/50'
           />
-        </Select.Trigger>
-        <Select.Portal>
-          <Select.Content
-            sideOffset={10}
-            position='popper'
-            className='fade-zoom border-brand bg-white-soft shadow-main violet:border-white-gray
-              dark:bg-black-deep w-min rounded-lg border p-4.5'>
-            <Select.Viewport className='flex flex-col gap-2'>
-              {filteredColumns?.map(column => (
-                <Select.Item
-                  key={column.id}
-                  className='group data-[highlighted]:text-brand hocus:text-brand
-                    violet:hocus:text-brand-violet violet:data-[highlighted]:text-brand-violet flex
-                    cursor-pointer items-center gap-2 text-base outline-none dark:text-white/30'
-                  value={column.id}>
-                  <p className='w-20 truncate'>{column.title}</p>
-                  <Icon
-                    name='arrow-circle'
-                    className='group-focus:stroke-brand violet:group-focus:stroke-brand-violet size-4
-                      stroke-black dark:stroke-white/50'
-                  />
-                </Select.Item>
-              ))}
-            </Select.Viewport>
-          </Select.Content>
-        </Select.Portal>
-      </Select.Root>
+        </SelectTrigger>
+        <SelectContent
+          sideOffset={10}
+          className='w-min'>
+          <SelectViewport className='flex flex-col gap-2'>
+            {filteredColumns?.map(column => (
+              <SelectItem
+                key={column.id}
+                className='group flex items-center gap-2'
+                value={column.id}>
+                <p className='w-20 truncate'>{column.title}</p>
+                <Icon
+                  name='arrow-circle'
+                  className='group-focus:stroke-brand violet:group-focus:stroke-brand-violet size-4
+                    stroke-black dark:stroke-white/50'
+                />
+              </SelectItem>
+            ))}
+          </SelectViewport>
+        </SelectContent>
+      </Select>
     )
   }
 )
