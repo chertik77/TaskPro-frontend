dcf27b9896541d90564247bad3c93697062b7b45
chertik77 <denysbabych.dev@gmail.com>
2025-05-28 15:48:22 +0100

    fix: add scroll for overlay

--- src/blocks/sidebar/SidebarMobileMenu.tsx
+++ src/blocks/sidebar/SidebarMobileMenu.tsx
@@ -26,7 +26,7 @@ export const SidebarMobileMenu = () => {
         <DialogOverlay
           className='data-[state=open]:animate-modal-overlay-in
             data-[state=closed]:animate-modal-overlay-out bg-black-overlay fixed inset-0
-            z-50'>
+            z-50 overflow-y-scroll'>
           <DialogContent
             aria-describedby={undefined}
             className='violet:bg-brand-violet dark:bg-black-soft tablet:w-[260px] tablet:pt-6
