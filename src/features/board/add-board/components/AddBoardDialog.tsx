18880c6db9ef4bf6b1fb477818c4c922b4a968f6
chertik77 <denysbabych.dev@gmail.com>
2025-05-28 03:07:35 +0100

    fix: add logic for preventing obstructed inputs on mobile

--- src/features/board/add-board/components/AddBoardDialog.tsx
+++ src/features/board/add-board/components/AddBoardDialog.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react'
 
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import {
   Dialog,
@@ -18,12 +18,36 @@ type AddBoardDialogProps = {
 export const AddBoardDialog = ({ children }: AddBoardDialogProps) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
 
+  const formContainerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (formContainerRef.current) {
+        formContainerRef.current.style.setProperty(
+          'bottom',
+          `env(safe-area-inset-bottom)`
+        )
+      }
+    }
+
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', handleResize)
+      handleResize() // Initial call in case the keyboard is already open
+    }
+
+    return () => {
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener('resize', handleResize)
+      }
+    }
+  }, [])
+
   return (
     <Dialog
       open={isDialogOpen}
       onOpenChange={setIsDialogOpen}>
       {children}
-      <DialogContent>
+      <DialogContent ref={formContainerRef}>
         <DialogTitle>New board</DialogTitle>
         <DialogDescription className='sr-only'>
           You can create a new board here by adding a title, icon and
