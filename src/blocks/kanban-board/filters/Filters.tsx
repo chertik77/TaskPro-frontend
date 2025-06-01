1510d5d7f4659ed883b116b43082b009d818b11c
chertik77 <denysbabych.dev@gmail.com>
2025-06-01 18:50:54 +0100

    feat: add autoFocus to filters when board is opened

--- src/blocks/kanban-board/filters/Filters.tsx
+++ src/blocks/kanban-board/filters/Filters.tsx
@@ -16,7 +16,9 @@ export const Filters = () => {
 
   return (
     <Popover>
-      <PopoverTrigger className='focus-visible:styled-outline desktop:mr-6 mr-5 flex items-center gap-2'>
+      <PopoverTrigger
+        className='focus-visible:styled-outline desktop:mr-6 mr-5 flex items-center gap-2'
+        autoFocus>
         <Icon
           name='filter'
           className='size-4 stroke-black/80 dark:stroke-white/80'
