4c8eea431f0c462054664744fb4e6b89e22994de
chertik77 <denysbabych.dev@gmail.com>
2025-06-01 18:31:32 +0100

    fix: destructure board id out of param

--- src/shared/hooks/useGetParamBoardId.ts
+++ src/shared/hooks/useGetParamBoardId.ts
@@ -1,12 +1,16 @@
 import { useParams } from '@tanstack/react-router'
 
 export const useGetParamBoardId = () => {
-  const boardId = useParams({
+  const params = useParams({
     from: '/(dashboard)/dashboard/$boardId',
     shouldThrow: false
   })
 
-  if (!boardId) throw new Error('Board id is not available on this route.')
+  const boardId = params?.boardId
+
+  if (!boardId) {
+    throw new Error('Board id is not available on this route.')
+  }
 
   return boardId
 }
