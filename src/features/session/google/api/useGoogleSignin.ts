eff97d0336ed7062a21c7d01fd0eca9584bf5f5f
chertik77 <denysbabych.dev@gmail.com>
2025-06-26 01:02:33 +0100

    feat: add loading while authenticating with google

--- src/features/session/google/api/useGoogleSignin.ts
+++ src/features/session/google/api/useGoogleSignin.ts
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useGoogleLogin } from '@react-oauth/google'
 import { useNavigate } from '@tanstack/react-router'
 import { toast } from 'sonner'
@@ -5,20 +6,27 @@ import { toast } from 'sonner'
 import { sessionService, useSessionStore } from '@/entities/session'
 
 export const useGoogleSignin = () => {
+  const [loading, setIsLoading] = useState(false)
+
   const { authenticate } = useSessionStore()
 
   const navigate = useNavigate()
 
-  return useGoogleLogin({
+  const signinWithGoogle = useGoogleLogin({
     flow: 'auth-code',
     onSuccess: async ({ code }) => {
+      setIsLoading(true)
       const r = await sessionService.signinWithGoogle({ code })
       authenticate(r)
       navigate({ to: '/dashboard' })
     },
-    onError: () =>
+    onError: () => {
+      setIsLoading(false)
       toast.error(
         'Authentication failed: Unable to sign in with Google. Please try again.'
       )
+    }
   })
+
+  return { signinWithGoogle, loading }
 }
--- src/features/session/google/ui/GoogleSignin.tsx
+++ src/features/session/google/ui/GoogleSignin.tsx
@@ -1,22 +1,32 @@
-import { Icon } from '@/shared/ui'
+import { Icon, Loader } from '@/shared/ui'
 
 import { useGoogleSignin } from '../api/useGoogleSignin'
 
 export const GoogleSignin = () => {
-  const signinWithGoogle = useGoogleSignin()
+  const { signinWithGoogle, loading } = useGoogleSignin()
 
   return (
     <button
       className='flex w-84 items-center justify-center gap-2 rounded-lg bg-black py-2.5
-        text-center text-white'
+        text-center text-white disabled:cursor-not-allowed'
       type='button'
+      disabled={loading}
       aria-label='Continue with Google'
       onClick={signinWithGoogle}>
-      <Icon
-        name='google'
-        className='size-7'
-      />
-      Continue with Google
+      {loading ? (
+        <>
+          <Loader />
+          Signing you in...
+        </>
+      ) : (
+        <>
+          <Icon
+            name='google'
+            className='size-7'
+          />
+          Continue with Google
+        </>
+      )}
     </button>
   )
 }
