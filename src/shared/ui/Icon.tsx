e56f3af201d8699c08bd8e9291f68a953658b9ce
chertik77 <denysbabych.dev@gmail.com>
2025-06-27 18:21:16 +0100

    fix: minor updates & improvements

--- src/shared/ui/Icon.tsx
+++ src/shared/ui/Icon.tsx
@@ -2,8 +2,10 @@ import type { ComponentProps } from 'react'
 
 import { cn } from '../lib'
 
-export const Icon = ({ name, className }: ComponentProps<'svg'>) => (
-  <svg className={cn('stroke-current', className)}>
+export const Icon = ({ name, className, ...props }: ComponentProps<'svg'>) => (
+  <svg
+    className={cn('stroke-current', className)}
+    {...props}>
     <use href={`/icons.svg#${name}`} />
   </svg>
 )
