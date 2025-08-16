6a62bce0a12bbf0407b8081af13c2e50d750b017
chertik77 <denysbabych.dev@gmail.com>
2025-08-16 18:57:35 +0100

    fix: minor updates & improvements

--- src/widgets/auth-navigation/ui/AuthNavigation.tsx
+++ src/widgets/auth-navigation/ui/AuthNavigation.tsx
@@ -1,22 +1,26 @@
+import {
+  RovingFocusGroup,
+  RovingFocusGroupItem
+} from '@radix-ui/react-roving-focus'
 import { Link } from '@tanstack/react-router'
 
 export const AuthNavigation = () => (
-  <ul className='mb-10 flex items-center gap-3.5 text-xl'>
-    <li>
+  <RovingFocusGroup className='mb-10 flex items-center gap-3.5 text-xl'>
+    <RovingFocusGroupItem asChild>
       <Link
         to='/auth/signup'
         className='focus-visible:styled-outline text-white/30
           aria-[current=page]:text-white'>
         Registration
       </Link>
-    </li>
-    <li>
+    </RovingFocusGroupItem>
+    <RovingFocusGroupItem asChild>
       <Link
         to='/auth/signin'
         className='focus-visible:styled-outline text-white/30
           aria-[current=page]:text-white'>
         Log In
       </Link>
-    </li>
-  </ul>
+    </RovingFocusGroupItem>
+  </RovingFocusGroup>
 )
